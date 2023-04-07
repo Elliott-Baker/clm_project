928fddf9e5d683b98e3263abe651506542c4cd99
Elliott-Baker <ecb0105@auburn.edu>
2023-04-06 22:33:28 -0500

    more changes

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import pandas as pd
 import plotly.graph_objects as go
 from dash import dcc, html
 
-df = pd.read_csv('https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/companies.csv')
-fig = go.Figure(data=[go.Histogram(x=df['Percent Change'])])
+csv_path =  'https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/numbers.csv'
+column_names = ['x', 'y']
+#df = pd.read_csv('https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/companies.csv')
+df = pd.read_csv(csv_path, usecols=column_names)
+print(df)
+
+fig = go.Figure(data=[go.Scatter(x=df['x'], y=df['y'])])
 
 app = dash.Dash()
 app.layout = html.Div([
