6cc8175c452c936da83b97b5fd853e4195e020a2
Elliott-Baker <ecb0105@auburn.edu>
2023-04-07 23:36:51 -0400

    removed whitespace

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import plotly.graph_objects as go
+import dash
 from dash import dcc, html
 
-csv_path =  'https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/numbers.csv'
-column_names = ['x', 'y']
+csv_path =  'https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/companies.csv'
+#csv_path = 'https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv'
+column_names = ['Company Change', 'Percent Change']
 #df = pd.read_csv('https://raw.githubusercontent.com/Elliott-Baker/clm_project/master/datasets/companies.csv')
 df = pd.read_csv(csv_path, usecols=column_names)
 print(df)
 
-fig = go.Figure(data=[go.Scatter(x=df['x'], y=df['y'])])
+fig = go.Figure(data=[go.Scatter(x=df['AAPL_x'], y=df['AAPL_y'])])
 
 app = dash.Dash()
 app.layout = html.Div([
